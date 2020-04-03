7c4ec4608a438bef249dc6b078cd6e1e5c02597b
Kunon <61319557+Kunon@users.noreply.github.com>
2020-04-03 23:23:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TargetObject : MonoBehaviour
+{
+    public GameObject goalGameObject;
+
+    void OnMouseDrag();
+    {
+      transform.position=new vector1(0, 5, 0);
+    }
+     private void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.Equals())
+       {
+        Debug.Log("Win");
+       }
+
+    }
+}
